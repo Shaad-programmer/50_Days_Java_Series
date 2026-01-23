a76f1083bfd360ec59ff0942fb128bbd2e007ce4
Mohd_Shaad <143491424+BC2023124@users.noreply.github.com>
2026-01-23 18:53:45 +0530

    Add files via upload

---
+++ day_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class day_1 {
+    
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!!\n");
+        System.out.println("SHAAD");
+        System.out.println("*\n**\n***\n****");
+        System.out.println("  *\n ***\n*****\n ***\n  *");
+        // variables
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int mul=a*b;
+        System.out.println(sum);
+        System.out.println(mul);
+        int p=10;
+        int q=5;
+        int ans=(p*q)/(p-q);
+        System.out.println(ans);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String acha = sc.next();
+
+        // nextInt();
+        // nextFloat();
+        
+        System.out.println(name);
+        System.out.println(acha);
+        int x= sc.nextInt();
+        int y= sc.nextInt();
+        int add= x+y;
+        System.out.println(add);
+        
+    }
+}
