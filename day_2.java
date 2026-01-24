a089d7aff1a7dd59df1d3f005ee9ad40bcca51c6
Mohd_Shaad <143491424+BC2023124@users.noreply.github.com>
2026-01-24 22:39:43 +0530

    Add files via upload
    Day 2 – #50DaysOfJava
    Covered conditional statements (if-else) and switch in Java.
    Building logic, one day at a time.

---
+++ day_2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class day_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button =sc.nextInt();
+        int age = sc.nextInt();
+        if(age>= 18){
+            System.out.println("adult");
+        } else {
+            System.out.println("not adult");
+        }
+
+        int num= sc.nextInt();
+        if(num%2 == 0){
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+        
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        if(a==b){
+            System.out.println("equal");
+        } else if(a>b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is lesser than b");
+        } 
+    
+        switch(button){
+            case 1 : System.out.println("hello");
+            break;
+            case 2 : System.out.println("Good Morning");
+            break;
+            case 3 : System.out.println("Bonjour");
+            break;
+            default: System.out.println("invalid button");
+        }
+    }
+}
