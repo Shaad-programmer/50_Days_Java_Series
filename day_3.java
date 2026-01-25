4dc87556a7ed2d5a7b1372889e7c4f2e87aa908f
Mohd_Shaad <143491424+Shaad-programmer@users.noreply.github.com>
2026-01-25 17:39:19 +0530

    Add files via upload
    Day 3 - #50 days of java
    covered for loop, while loop, do while loop in java.
    practicing logic through repetition.
    #Java #java #Javabasics #Learninginpublic #codingjourney

---
+++ day_3.java
@@ -0,0 +1,28 @@
+public class day_3 {
+    public static void main(String[] args) {
+        // for loop
+        for(int i = 0; i< 3; i = i+1){
+            System.out.println("hello world");
+        }
+        System.out.println();
+        for(int a=0; a<= 3; a++){
+            System.out.println(a);
+        }
+
+        System.out.println();
+        // while loop
+        int i=0;
+        while(i< 11){
+            System.out.println(i);
+            i=i+1;
+        }
+
+        System.out.println();
+        // do while loop
+        // int i=0;
+        do{
+            System.out.println(i);
+            i=i+1;
+        } while(i<11);
+    }
+}
