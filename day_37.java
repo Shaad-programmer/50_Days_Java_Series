ff2da581286728d8e0e4355a1d78e9fae5ed66ec
Mohd_Shaad <143491424+Shaad-programmer@users.noreply.github.com>
2026-03-01 21:21:11 +0530

    Add files via upload

---
+++ day_37.java
@@ -0,0 +1,58 @@
+import java.util.Stack;
+//To push an element at the bottom of a stack
+public class day_37 {
+    // public static void pushAtBottom(Stack<Integer> s, int data) {
+    //     if(s.isEmpty()) {
+    //         s.push(data);
+    //         return;
+    //     }
+
+    //     int temp = s.pop();
+    //     pushAtBottom(s, data);
+    //     s.push(temp);
+    // }
+
+    // public static void main(String args[]) {
+    //     Stack<Integer> stack = new Stack<>();
+    //     stack.push(1);
+    //     stack.push(2);
+    //     stack.push(3);
+    //     pushAtBottom(stack, 4);
+
+    //     while(!stack.isEmpty()) {
+    //         System.out.println(stack.pop());
+    //     }
+    // }
+
+    //Code to Reverse a Stack
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int temp = s.pop();
+        pushAtBottom(s, data);
+        s.push(temp);
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if(s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
